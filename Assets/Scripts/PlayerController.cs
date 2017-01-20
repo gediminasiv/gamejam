361cc5fa5c6a331ca1929046af55b13de3cc8d23
Gediminas Ivanauskas <gediminas@mediapark.lt>
2017-01-21 00:43:59 +0200

    Add movement to body

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+	public float Speed = 0f;
+	private float movex = 0f;
+	private float movey = 0f;
+	Rigidbody2D rigidbody2D;
+//	public rigidbody = new Rigidbody("Player")
 	// Use this for initialization
 	void Start () {
-		
+		rigidbody2D = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void FixedUpdate() {
+		movex = Input.GetAxis ("Horizontal");
+		movey = Input.GetAxis ("Vertical");
+		rigidbody2D.velocity = new Vector2 (movex * Speed, movey * Speed);
 	}
 }
